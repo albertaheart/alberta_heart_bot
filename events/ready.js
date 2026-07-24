fffb994f22d0303965fde6a1d19c17c2ac2130d3
Auddiee <oniaudrey106@gmail.com>
2026-07-24 17:19:19 -0600

    made some changes

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,9 @@
-const { Events } = require('discord.js');
+
+
+const {Events, ActivityType} = require('discord.js');
 
 const readyEvent = (client) => {
+  client.user.setActivity('Use /ask for AB Heart help', { type: ActivityType.Watching });
   console.log(`Ready! Logged in as ${client.user.tag}`);
 };
 
--- index.js
+++ index.js
@@ -40,10 +40,5 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args));
   }
 }
-const { ActivityType } = require('discord.js');
 
-client.once('ready', () => {
-    client.user.setActivity('Use /ask for AB Heart help', { type: ActivityType.Watching });
-    console.log(`Logged in as ${client.user.tag}`);
-});
 client.login(process.env.DISCORD_TOKEN);
